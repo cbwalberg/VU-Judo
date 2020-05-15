8ab62b381adfc465dc095110166f8bc173341e36
Charles Walberg <charlesbwalbergg@gmail.com>
2020-05-14 22:41:26 -0400

    Working on LogActivity...

--- app/src/main/java/vu/judo/app/LogActivity.java
+++ app/src/main/java/vu/judo/app/LogActivity.java
@@ -1,5 +1,6 @@
 package vu.judo.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
@@ -12,23 +13,39 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Calendar;
+import java.util.Map;
 
 public class LogActivity extends AppCompatActivity {
 
     static final String TAG = "Log";
 
-    int selectedDay, selectedMonth, selectedYear;
-    String exercise;
+    int reps, wazaModifier = 50, exerciseModifier = 10, selectedDay, selectedMonth, selectedYear;
+    String exercise, selectedDate, userEmail;
 
     TextView exerciseView, dateView;
-    EditText reps;
+    EditText repsInput;
 
     Calendar calendar;
 
-    @Override
-    @SuppressWarnings("ConstantConditions")
+    FirebaseFirestore db;
+    DocumentReference userDoc, exerciseDoc;
+    CollectionReference dateCollection;
+
+
+    @Override @SuppressWarnings("ConstantConditions")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
@@ -38,7 +55,7 @@ public class LogActivity extends AppCompatActivity {
             Log.e(TAG, "getSupportActionBar().setDisplayHomeAsUpEnabled:failure", e);
         }
         exerciseView = findViewById(R.id.logExercise);
-        reps = findViewById(R.id.logReps);
+        repsInput = findViewById(R.id.logReps);
         dateView = findViewById(R.id.logDate);
 
         //Get the exercise passed to this activity from calling activity
@@ -105,6 +122,7 @@ public class LogActivity extends AppCompatActivity {
                                 }
                                 String date = monthText + " " + day + " " + year;
                                 dateView.setText(date);
+                                selectedDate = date;
                             }
                         },
                         selectedYear, selectedMonth, selectedDay);
@@ -119,11 +137,54 @@ public class LogActivity extends AppCompatActivity {
         super.onStart();
 
         //Clear any existing text
-        reps.getText().clear();
+        repsInput.getText().clear();
     }
 
     public void save(View view) {
-        //...
+        // Find current user, check if Practice Log collection already exists, if so add to it, if not, create collection within user document named Practice Log
+        // Add new document to Practice Log with name "selectedDate". Fields in selectedDate document are names of waza/exercises, values of the fields are the number of reps
+        db = FirebaseFirestore.getInstance();
+
+        userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        reps = Integer.parseInt(repsInput.getText().toString());
+
+        userDoc = db.collection("users").document(userEmail);
+        userDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override @SuppressWarnings("ConstantConditions")
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot userDocument = task.getResult();
+                    if (userDocument.exists()) {
+                        dateCollection = db.collection("Practice Log");
+                        dateCollection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult().size() > 0) { //Collection of name "Practice Log" exists, check for field "exercise" in document "selectedDate", if it exists, read its value
+                                        for (QueryDocumentSnapshot dateDocument : task.getResult()) {
+                                            if (dateDocument.getString("date").equals(selectedDate)) {
+                                                //...
+                                            }
+                                        }
+                                    } else { //No collection of name "Practice Log" exists, create a new one
+                                        //...
+                                    }
+                                } else {
+                                    Toast.makeText(LogActivity.this, "Failed to query database. Please restart the application", Toast.LENGTH_LONG).show();
+                                    Log.d(TAG, "get failed with ", task.getException());
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(LogActivity.this, "Failed to find user information. Please restart the application", Toast.LENGTH_LONG).show();
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Toast.makeText(LogActivity.this, "Failed to find user information. Please restart the application", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
     @Override
