6f71423aeb93c07a4fd3107360ccde9978bce141
Charles Walberg <charlesbwalbergg@gmail.com>
2020-07-27 15:42:59 -0400

    Working on server-side functionality

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,25 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const db = admin.firestore();
+
 // [Function purpose]
-// Runs at 12AM ET every Sunday
-exports.scheduledFunctionCronTab = functions.pubsub.schedule('0 19 * * 6').timeZone('America/New_York').onRun((context) => {
-  console.log('Testing a scheduled function');
-  // Input functionality
+// Runs at 12AM ET every Sunday '0 0 * * 0'. Using '*/5 * * * *' (every five minutes) for testing purposes
+exports.scheduledFunctionCronTab = functions.pubsub.schedule('*/5 * * * *').timeZone('America/New_York').onRun((context) => {
+
+  const queryRef = db.collection('users');
+  /*const getQuery = queryRef.get().then(query => {
+    let docs = query.docs;
+    for (var i=0; i<docs.length; i++) {
+      console.log(docs[i].data());
+    }
+    return res.send(query.docs);
+  });*/
+
+  let now = new Date();
+  let month = now.getMonth() + 1;
+  let leaderboardDocName = 'leaderboard/Week of ' + month + '-' + now.getDate().toString() + '-' + now.getFullYear().toString();
+  console.log(leaderboardDocName);
+
   return null;
 });
