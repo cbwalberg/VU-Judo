f11fbce0a3f245ad90c179eee6012ae0c29cc15b
Charles Walberg <charlesbwalbergg@gmail.com>
2020-07-30 23:51:57 -0400

    Get all user data from db and sort in descending order by score

--- functions/index.js
+++ functions/index.js
@@ -8,17 +8,27 @@ admin.initializeApp();
 const db = admin.firestore();
 
 // [Function purpose]
-// Runs at 12AM ET every Sunday '0 0 * * 0'. Using '*/5 * * * *' (every five minutes) for testing purposes
+// Runs at 12AM ET every Sunday '0 0 * * 0'
+// Using '*/5 * * * *' (every five minutes) for testing purposes
 exports.scheduledFunctionCronTab = functions.pubsub.schedule('*/5 * * * *').timeZone('America/New_York').onRun((context) => {
 
   const queryRef = db.collection('users');
-  /*const getQuery = queryRef.get().then(query => {
+  var scoreArray = [], top3 = [];
+
+  const getQuery = queryRef.get().then(query => {
     let docs = query.docs;
-    for (var i=0; i<docs.length; i++) {
-      console.log(docs[i].data());
+    for (let i=0; i<docs.length; i++) {
+      scoreArray.push(docs[i].data());    
     }
-    return res.send(query.docs);
-  });*/
+
+    scoreArray.sort((a,b) => {return b.score - a.score});
+    for (let i=0; i<3; i++) {
+      top3.push(scoreArray[i])
+      console.log(`Retrieved data: ${JSON.stringify(top3[i])}`);
+    }
+
+    return null;
+  });
 
   let now = new Date();
   let month = now.getMonth() + 1;
